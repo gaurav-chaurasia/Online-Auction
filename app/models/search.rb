fdd1442077d6fb17e25af3ff0aa3743027f9c8f4
Upendra <upendrapal.181me282@nitk.edu.in>
2020-06-23 14:09:34 +0530

    enhance search

--- app/models/search.rb
+++ app/models/search.rb
@@ -2,10 +2,16 @@ class Search < ApplicationRecord
     def search_auctions
         auctions = Auction.all
 
-        auctions = Auction.where(["name LIKE ?", "%#{keywords}%"]) if keywords.present?
-        auctions = Auction.where(["current_price >= ?", min_price]) if min_price.present?
-        auctions = Auction.where(["current_price <= ?", max_price]) if max_price.present?
-
+        if keywords.present?
+            auctions = Auction.where(["name LIKE ?", "%#{keywords}%"])
+            auctions = Auction.where(["current_price >= ? and name LIKE ?", min_price, "%#{keywords}%"]) if min_price.present? and !max_price.present?
+            auctions = Auction.where(["current_price <= ? and name LIKE ?", max_price, "%#{keywords}%"]) if max_price.present? and !min_price.present?
+            auctions = Auction.where(["current_price <= ? and current_price >= ? and name LIKE ?", max_price, min_price, "%#{keywords}%"]) if max_price.present? and min_price.present?
+        else 
+            auctions = Auction.where(["current_price >= ?", min_price]) if min_price.present? and !max_price.present?
+            auctions = Auction.where(["current_price <= ?", max_price]) if max_price.present? and !min_price.present?
+            auctions = Auction.where(["current_price <= ? and current_price >= ?", max_price, min_price]) if max_price.present? and min_price.present?
+        end
         return auctions
     end
 end
